b95abdad517e5c4772eef014871637a34da2f84b
HToan <huutrantoan@gmail.com>
2021-10-31 17:53:47 +0700

    Navigation

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import About from "./screens/About";
+import ReviewDetails from "./screens/ReviewDetails";
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Home />
+      <About />
+      <ReviewDetails />
+    </NavigationContainer>
+  );
 }
