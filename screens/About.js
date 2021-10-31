d9293ba497b3da99b4e520860e881330fa914bd0
HToan <huutrantoan@gmail.com>
2021-10-31 14:35:58 +0700

    Init

---
+++ screens/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default About = () => {
+  return (
+    <View style={styles.container}>
+      <Text>About</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
